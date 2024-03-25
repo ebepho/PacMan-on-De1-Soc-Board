e879ddfb048f4e16f4c48aa351c4457ec75b1912
ebepho <powus2@gmail.com>
2024-03-25 11:46:40 -0400

    Removed .h files since they don't work with assembly well

--- ps2.c
+++ ps2.c
@@ -1,5 +1,11 @@
-#include "ps2.h"
+unsigned char byte1 = 0;
+unsigned char byte2 = 0;
+unsigned char byte3 = 0;
+volatile int * PS2_ptr = (int *) 0xFF200100;
+int PS2_data, RVALID;
+#define RLEDs ((volatile long *) 0xFF200000)
 
+void PS2_ISR();
 void PS2_ISR(void) {
     PS2_data = *(PS2_ptr);	// read the Data register in the PS/2 port
     RVALID = (PS2_data & 0x8000);	// extract the RVALID field
--- ps2.h
+++
@@ -1,8 +0,0 @@
-unsigned char byte1 = 0;
-unsigned char byte2 = 0;
-unsigned char byte3 = 0;
-volatile int * PS2_ptr = (int *) 0xFF200100;
-int PS2_data, RVALID;
-#define RLEDs ((volatile long *) 0xFF200000)
-
-void PS2_ISR();
