91a0e0a02011b88619e65643e80e99b2ce1ea55a
ebepho <91153139+ebepho@users.noreply.github.com>
2024-04-01 11:14:22 -0400

    Update test.c

--- test.c
+++ test.c
@@ -12768,8 +12768,6 @@ struct Player {
   int dx;
   int dy;
 
-  int width;
-
   unsigned short *sprite[3];
 
   int sprite_num;
@@ -12789,8 +12787,6 @@ struct Ghost {
   unsigned int timer;
   bool edible;
   bool jail;
-
-  int width;
 
   unsigned short *sprite_normal[1];
   unsigned short *sprite_edbile[1];
@@ -12822,6 +12818,7 @@ struct Timer {
 
 // -------------------------- GLOBALS.H --------------------------
 // Map
+short int Map[240][512];
 int game_countdown;
 bool game_over;
 
@@ -12908,8 +12905,6 @@ void game_setup() {
   player1.x_prev = player1.x;
   player1.y_prev = player1.y;
 
-  player1.width = 16;
-
   player1.sprite[0] = pac_000;
   player1.sprite[1] = pac_000;
   player1.sprite[2] = pac_000;
@@ -12921,7 +12916,6 @@ void game_setup() {
     ghosts[i].dy = 0;
     ghosts[i].edible = false;
     ghosts[i].jail = true;
-    ghosts[i].width = 16;
     ghosts[i].timer = i * 4 * 100000000;
     ghosts[i].sprite_normal[0] = ghost_1;
   }
@@ -13108,39 +13102,21 @@ void ghost_ai() {
         }
       }
     } else {
-        // Random movement - CHANGE TO MAKE IT MOVE RANDOMLY ONLY WHEN YOU REACH A JUNCTION
-        int num_available_moves = 0;
-        int available_moves[4][2];
-
-        for (int i = 0; i < 4; i++) {
-            if (!(map[ghosts[i].x + 1 + ghosts[i].width][ghosts[i].y + 0 + ghosts[i].width] != 0x0000)) {
-                available_moves[num_available_moves][0] = 1;
-                available_moves[num_available_moves][1] = 0;
-                num_available_moves++;
-            }
-
-            if (!(map[ghosts[i].x - 1 - ghosts[i].width][ghosts[i].y + 0 + ghosts[i].width] != 0x0000)) {
-                available_moves[num_available_moves][0] = -1;
-                available_moves[num_available_moves][1] = 0;
-                num_available_moves++;
-            }
-
-            if (!(map[ghosts[i].x + 0 + ghosts[i].width][ghosts[i].y + 1 + ghosts[i].width] != 0x0000)) {
-                available_moves[num_available_moves][0] = 0;
-                available_moves[num_available_moves][1] = 1;
-                num_available_moves++;
-            }
-
-            if (!(map[ghosts[i].x + 0 + ghosts[i].width][ghosts[i].y - 1 - ghosts[i].width] != 0x0000)) {
-                available_moves[num_available_moves][0] = 0;
-                available_moves[num_available_moves][1] = -1;
-                num_available_moves++;
-            }
-
-            int rand_num = rand() % num_available_moves;
-            ghosts[i].dx = available_moves[rand_num][0];
-            ghosts[i].dy = available_moves[rand_num][1];
-        }
+      // Random movement - CHANGE TO MAKE IT MOVE RANDOMLY ONLY WHEN YOU REACH A JUNCTION
+      int rand_num = rand() % 4;
+      if (rand_num == 0) {
+        ghosts[i].dx = 0;
+        ghosts[i].dy = 1;
+      } else if (rand_num == 1) {
+        ghosts[i].dx = 0;
+        ghosts[i].dy = -1;
+      } else if (rand_num == 2) {
+        ghosts[i].dx = 1;
+        ghosts[i].dy = 0;
+      } else if (rand_num == 3) {
+        ghosts[i].dx = -1;
+        ghosts[i].dy = 0;
+      }
     }
   }
 }
